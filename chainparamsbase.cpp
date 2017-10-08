e4ece8203ed21d7642cab227b3d5213c3d134095
neener365 <32602355+neener365@users.noreply.github.com>
2017-10-08 14:34:25 -0700

    Add files via upload

--- chainparamsbase.cpp
+++ chainparamsbase.cpp
@@ -22,7 +22,7 @@ public:
     CBaseMainParams()
     {
         networkID = CBaseChainParams::MAIN;
-        nRPCPort = 9332;
+        nRPCPort = 8822;
     }
 };
 static CBaseMainParams mainParams;
@@ -36,7 +36,7 @@ public:
     CBaseTestNetParams()
     {
         networkID = CBaseChainParams::TESTNET;
-        nRPCPort = 9332;
+        nRPCPort = 8824;
         strDataDir = "testnet4";
     }
 };
